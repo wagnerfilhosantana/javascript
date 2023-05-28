cdd8c8fa2ef616f6f51ad14cd44b08104e7293bf
WagnerFilhoSantana <wagnersantanadasilva3@gmail.com>
2023-05-28 15:58:53 -0300

    Initial commit

---
+++ cursosjs/aula13/teste.js
@@ -0,0 +1,18 @@
+var c = 1
+/*while (c <= 100) {
+
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}*/
+
+
+
+
+
+/*
+console.log('Tudo bem?')*/
+do {
+    console.log(`Passo ${c}`)
+    c++
+}
+while (c <= 100)
---
+++ cursosjs/aula16/funcoes.js
@@ -0,0 +1,36 @@
+function parimp(n) {
+    if (n%2 == 0){
+        return 'par'
+    }
+    else{
+        return 'impar'
+    }
+}
+console.log(parimp(64))
+/*let res = parimp(5)
+console.log(res)*/
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+console.log(soma(7, 8))
+let v = function (x){
+    return x*2
+}
+console.log(v(5))
+function fatorial(n) {
+    let fat = 1
+    for(let c = n; c > 1; c--) {
+        fat *= c
+    }
+    return fat
+}
+    console.log(fatorial(7))
+    function fatorial(n) {
+        if (n == 1) {
+            return 1 
+        }
+        else { 
+            return n * fatorial(n-1)
+        }
+    }
+    console.log(fatorial (9))
