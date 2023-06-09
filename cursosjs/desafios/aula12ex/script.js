9494a4e87252eedb2f31a1d19ec417075dfe642a
WagnerFilhoSantana <wagnersantanadasilva3@gmail.com>
2023-06-09 20:05:39 -0300

    Interação com imagens

---
+++ cursosjs/desafios/aula12ex/script.js
@@ -0,0 +1,19 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'manha.png'
+    }
+     else if (hora >= 12 && hora < 18) {
+        //BOA TARDE!
+        img.src = 'tarde.png'
+    }
+    else {
+        //BOA NOITE!
+        img.src = 'noite.png'
+    }
+}
