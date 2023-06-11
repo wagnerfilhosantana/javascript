b45a041c9b2ff28b457204bd367f2a9e8a118386
WagnerFilhoSantana <wagnersantanadasilva3@gmail.com>
2023-06-11 11:50:10 -0300

    Projeto Finalizado

--- cursosjs/desafios/aula12ex/script.js
+++ cursosjs/desafios/aula12ex/script.js
@@ -3,8 +3,7 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    //msg.innerHTML = `Agora são ${hora} horas.`
-    var hora = 19
+    msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //BOM DIA!
         img.src = 'manha.png'
