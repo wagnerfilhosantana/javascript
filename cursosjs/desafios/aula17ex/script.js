62655b1382d9418e6e23f083238fac9d8324c24b
WagnerFilhoSantana <wagnersantanadasilva3@gmail.com>
2023-06-15 20:36:31 -0300

    Avaliador de Números

--- cursosjs/desafios/aula17ex/script.js
+++ cursosjs/desafios/aula17ex/script.js
@@ -24,9 +24,42 @@ function inlista(n, l) {
 
 function adicionar()  {
     if (isNummero(num.value) && !inlista(num.value, valores)){
-        window.alert('Tudo ok!')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
    }
    else {
     window.alert('Valor inválido ou já encontrado na lista')
    }
+   num.value = ''
+   num.focus()
+}
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione um npumero antes de finalizar.')
+    }
+    else {
+        let tot = valores.length
+        res.innerHTML = ''
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores)
+         {
+           soma += valores[pos]
+        if (valores[pos] > maior)
+        maior = valores[pos]
+        else if (valores[pos] < menor)
+        menor = valores[pos]
+    }
+        media = soma / tot
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando os valores, temos ${soma}.`
+        res.innerHTML += `<p>A média desses números são ${media}.</p>`
+    }
 }
