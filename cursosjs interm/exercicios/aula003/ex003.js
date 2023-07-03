13adccc91ff9d0f31acd9ff444a6e461cdb77336
WagnerFilhoSantana <wagnersantanadasilva3@gmail.com>
2023-07-03 20:53:41 -0300

    Novo Curso de javascript

---
+++ cursosjs interm/exercicios/aula003/ex003.js
@@ -0,0 +1,11 @@
+'use strict' 
+function teste() {
+    let nome = 'Wagner'
+    if(true) {  
+        console.log('Dentro do IF do teste:' + nome)
+}
+    console.log('Dentro do teste:' + nome)
+}                               
+teste()
+const idade = 19
+console.log(idade)
---
+++ cursosjs interm/exercicios/aula004/ex004.js
@@ -0,0 +1,7 @@
+let num1 =30,num2= 20,res=num1+num2
+let n1,n2,n3
+n1=n2=n3=10
+n1++
+n2--
+console.log(res)
+console.log(n1,n2)
