04b246d86e4195fe5d15a76a261d9ce83ec880de
Wagner Filho <wagnersantanadasilva3@gmail.com>
2023-07-20 20:00:59 -0300

    curso de javascript

--- cursosjs interm/exercicios/aula009/ex09.js
+++ cursosjs interm/exercicios/aula009/ex09.js
@@ -1,7 +1,7 @@
-let n1=10
+let n1=-10
 let n2=20
 
-n1++
+let x=-n1
 
 console.log(n1)
-console.log(n2++)
+console.log(++n2)
---
+++ cursosjs interm/exercicios/aula010/ex010.js
@@ -0,0 +1,5 @@
+let num1= 10
+let num2= 5
+let res=(num1 > num2 ? "Verdadeiro" : "Falso")
+
+console.log(res)
