8a2e4d2dd868c834be2a8bff3613047503fb5bf2
Wagner Filho <wagnersantanadasilva3@gmail.com>
2023-07-23 18:30:23 -0300

    exercicio

---
+++ cursosjs interm/exercicios/aula014/ex14.js
@@ -0,0 +1,20 @@
+let semanas = 4
+
+switch(semanas) {
+    case 1: console.log("Domingo-Feira")
+    break
+    case 2: console.log("Segunda-Feira")
+    break
+    case 3: console.log("Terça-Feira")
+    break
+    case 4: console.log("Quarta-Feira")
+    break
+    case 5: console.log("Quinta-Feira")
+    break
+    case 6: console.log("Sexta-Feira")
+    break
+    case 7: console.log("Sábado-Feira")
+    break
+    default: console.log("Esse dia da semana não existe")
+    break
+}
